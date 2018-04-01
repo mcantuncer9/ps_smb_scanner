161b5a931e0048af8cab34781e89119a695eced5
mcantuncer9 <mcantuncerr@gmail.com>
2018-04-01 08:59:39 +0300

    Improvements will be added after April 6th.

--- README.md
+++ README.md
@@ -4,20 +4,22 @@ This script scans the inner network for open SMB Protocol with given username & 
 
 
 
-Needed parameters added to .ps1 file. (Turtle)
 
-ps_smb_scanner is a post exploitation tool for pen tests.
+
+ps_smb_scanner is a post exploitation tool for pen tests. 
+After server exploitation attacker can execute this tool with find All active machines in the domain and check if the given username & password work on that domain participants. Main purpose in this tool is use the server as a "Pivot."
+
 
 Work on machine:
-	~Windows7
-	~Enterprise
+	~Windows Server 2016
 	~64Bit Architecture
 
 Created a local network & domain with Windows Server 16:
-  - 3 users added to domain.
-  - Still having a problem for pinging same domain computers each other.
-	
-	
+  - 3 users added to domain. 
+  - Saved credentials test in code. 
+  - Need some progress for output. (Mostly OK.)  
+  - 1 user Windows7; 1 user Windows8; 1 user Windows10;
 	
 Mahmut Can Tuncer
+TOBB Economics & Technology University
 131101071
--- ps_smb_scanner.ps1
+++ ps_smb_scanner.ps1
@@ -1,19 +1,75 @@
-function ps_smb_scanner {
-    [CmdletBinding()] Param(
-	
-	  [parameter(Mandatory = $True)]
-      [String] $UserName,
+[CmdletBinding()] Param(
 
-      [parameter(Mandatory = $True)]
-      [String] $Password,
+        [parameter(Mandatory = $True)]
+        [String] $UserName,
+
+        [parameter(Mandatory = $True)]
+        [String] $Password
+
     )
 
     Begin {
         Set-StrictMode -Version 2
+        #try to load assembly
         Try {Add-Type -AssemblyName System.DirectoryServices.AccountManagement}
         Catch {Write-Error $Error[0].ToString() + $Error[0].InvocationInfo.PositionMessage}
     }
-	
-	
 
-}
+    Process {
+
+        $ComputerNames = @()
+        
+        Write-Verbose "Searching the domain for active machines."
+        "Searching the domain for active machines."
+
+        $ComputerNames = [array] ([adsisearcher]'objectCategory=Computer').Findall() | ForEach {$_.properties.cn}
+
+        Write-Verbose "Retrived $($ComputerNames.Length) systems from the domain."
+	"Retrived $($ComputerNames.Length) systems from the domain."
+
+        foreach ($Computer in $ComputerNames){     
+
+            Try {
+                
+                Write-Verbose "Checking: $Computer"
+                $up = Test-Connection -count 1 -Quiet $Computer 
+
+                if($up){
+
+                    if ($Username.contains("\\")) {
+                        $ContextType = [System.DirectoryServices.AccountManagement.ContextType]::Domain
+                    }
+                    else{
+                        $ContextType = [System.DirectoryServices.AccountManagement.ContextType]::Machine
+                    }
+
+		
+		$pc = New-Object DirectoryServices.AccountManagement.PrincipalContext($ContextType,$Computer)
+		$secString = ConvertTo-SecureString $Password -AsPlainText -Force
+#		Write-Host "$secString"
+#		Write-Host "$($pc.ValidateCredentials($Username, $Password))"
+		try {
+		    if($pc.ValidateCredentials($Username, $secString)) {
+                        Write-Verbose "[+] SUCCESS: $Username works with $Password on $Computer"
+				"[+] SUCCESS: $Username works with $Password on $Computer"
+
+                        $out = new-object psobject
+                        $out | add-member Noteproperty 'ComputerName' $Computer
+                        $out | add-member Noteproperty 'Username' $Username
+                        $out | add-member Noteproperty 'Password' $Password
+                        $out
+
+		    }
+		    else {
+                        Write-Verbose "[-] FAILURE: $Username did not work with $Password on $Computer"
+			"[-] FAILURE: $Username did not work with $Password on $Computer"
+ 		    }
+		}
+		finally {
+		    $pc.Dispose()
+	       }
+             }
+           }
+          Catch {Write-Error $($Error[0].ToString() + $Error[0].InvocationInfo.PositionMessage)}
+       }
+    }
